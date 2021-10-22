5c91242641771e6f2b41c5a8f7a4e48a523ffb5c
wangdy-code <1158046762@qq.com>
2021-10-22 15:44:35 +0800

    add 2 solutcion 4 118 556

--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"leetcode-go/leetcode/structures"
+	"sort"
 )
 
 type ListNode = structures.ListNode
 
 func main() {
-	set := map[int]struct{}{}
-	println(set[0])
-	set[1] = struct{}{}
-	println(set[1])
+	num1 := []int{1, 2}
+	nums2 := []int{3, 4}
 
+	println(findMedianSortedArrays1(num1, nums2))
 }
 func maxSubArray(nums []int) int {
 	max := nums[0]
@@ -25,13 +25,83 @@ func maxSubArray(nums []int) int {
 	}
 	return max
 }
-func containsDuplicate1(nums []int) bool {
-	set := map[int]struct{}{}
-	for _, v := range nums {
-		if _, has := set[v]; has {
-			return true
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+	m, n := len(nums1), len(nums2)
+	index1, index2 := 0, 0
+	res := make([]int, 0, m+n)
+	for {
+		if index1 == m {
+			res = append(res, nums2[index2:]...)
+			break
 		}
-		set[v] = struct{}{}
+		if index2 == n {
+			res = append(res, nums1[index1:]...)
+			break
+		}
+		if nums1[index1] < nums2[index2] {
+			res = append(res, nums1[index1])
+			index1++
+		} else {
+			res = append(res, nums2[index2])
+			index2++
+		}
 	}
-	return false
+	if (m+n)%2 != 0 {
+		return float64(res[(m+n)/2])
+	} else {
+		return float64((res[(m+n)/2] + res[(m+n)/2-1])) / float64(2)
+	}
+}
+func findMedianSortedArrays1(nums1 []int, nums2 []int) float64 {
+
+	m, n := len(nums1), len(nums2)
+	index, index1, index2 := 0, 0, 0
+	isOdd := false
+	if (m+n)%2 == 0 {
+		isOdd = true
+	}
+	for {
+		if index1 == m {
+			if index == (m+n)/2 {
+				if isOdd {
+					if index2 == 0 {
+						return (float64(nums2[index2]) + float64(nums1[m-1])) / 2
+					} else {
+						return (float64(nums2[index2]) + float64(nums2[index2-1])) / 2
+					}
+				} else {
+					return float64(nums2[index2])
+				}
+			}
+			index2++
+			index++
+			continue
+		}
+		if index2 == n {
+			if index == (m+n)/2 {
+				if isOdd {
+					if index2 == 0 {
+						return (float64(nums1[index1]) + float64(nums2[n-1])) / 2
+					} else {
+						return (float64(nums1[index1]) + float64(nums1[index1-1])) / 2
+					}
+				} else {
+					return float64(nums1[index1])
+				}
+			}
+			index1++
+			index++
+			continue
+		}
+		if nums1[index1] < nums2[index2] {
+			index1++
+			index++
+		} else {
+			index2++
+			index++
+		}
+	}
+
 }
