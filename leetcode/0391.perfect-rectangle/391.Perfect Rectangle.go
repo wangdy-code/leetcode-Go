4a0b74ca845c2794b3379919530ede181ed42bce
wdy <1158046762@qq.com>
2021-11-17 17:30:47 +0800

    add 391.perfect rectangle 1 solution

---
+++ leetcode/0391.perfect-rectangle/391.Perfect Rectangle.go
@@ -0,0 +1,48 @@
+package leetcode
+
+func isRectangleCover(rectangles [][]int) bool {
+	type point struct{ x, y int }
+	area, minX, minY, maxX, maxY := 0, rectangles[0][0], rectangles[0][1], rectangles[0][2], rectangles[0][3]
+	cnt := map[point]int{}
+	for _, rect := range rectangles {
+		x, y, a, b := rect[0], rect[1], rect[2], rect[3]
+		area += (a - x) * (b - y)
+
+		minX = min(minX, x)
+		minY = min(minY, y)
+		maxX = max(maxX, x)
+		maxY = max(maxY, y)
+		cnt[point{x, y}]++
+		cnt[point{x, b}]++
+		cnt[point{a, y}]++
+		cnt[point{a, b}]++
+
+	}
+	if area != (maxX-minX)*(maxY-minY) || cnt[point{minX, minY}] != 1 || cnt[point{maxX, maxY}] != 1 {
+		return false
+	}
+	delete(cnt, point{minX, minY})
+	delete(cnt, point{minX, maxY})
+	delete(cnt, point{maxX, minY})
+	delete(cnt, point{maxX, maxY})
+
+	for _, c := range cnt {
+		if c != 2 && c != 4 {
+			return false
+		}
+	}
+	return true
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
