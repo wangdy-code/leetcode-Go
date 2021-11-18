09cae1bec5d5b6b6aa51fd44aa5ec92013e8b439
wdy <1158046762@qq.com>
2021-11-18 23:30:38 +0800

    9.palindrome number 3 solution

--- leetcode/0009.palindrome-number/9.Palindrome Number.go
+++ leetcode/0009.palindrome-number/9.Palindrome Number.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "strconv"
+
 func isPalindrome(x int) bool {
 	if x < 0 {
 		return false
@@ -11,6 +13,29 @@ func isPalindrome(x int) bool {
 	}
 	for i := 0; i < len(nums); i++ {
 		if nums[i] != nums[len(nums)-1-i] {
+			return false
+		}
+	}
+	return true
+}
+func isPalindrome1(x int) bool {
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
+	}
+	revertedNumber := 0
+	for x > revertedNumber {
+		revertedNumber = revertedNumber*10 + x%10
+		x /= 10
+	}
+	return revertedNumber == x || x == revertedNumber/10
+}
+func isPalindrome2(x int) bool {
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
+	}
+	str := strconv.FormatInt(int64(x), 10)
+	for i := 0; i < len(str)/2+1; i++ {
+		if str[i] != str[len(str)-1-i] {
 			return false
 		}
 	}
